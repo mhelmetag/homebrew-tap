50ebfa659f2c682e35fc219ab8d18e5709622574
Max Helmetag <mhelmetag@gmail.com>
2018-09-18 22:39:10 -0700

    fix link url

--- gosurf.rb
+++ gosurf.rb
@@ -1,7 +1,7 @@
 class Gosurf < Formula
   desc "CLI Client for Surfline"
   homepage "https://github.com/mhelmetag/gosurf"
-  url "https://github.com/mhelmetag/gosurf/archive/0.0.4/gosurf_darwin_amd64"
+  url "https://github.com/mhelmetag/gosurf/releases/download/0.0.4/gosurf_darwin_amd64"
   sha256 "e46634283c047c71c1aaec05a0fe66ac1504958a5b21b027a4a825641ac6ef13"
 
   def install
