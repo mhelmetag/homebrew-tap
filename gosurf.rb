9518a1db8c79777edfba8bb41079f0df090d3e8c
Max Helmetag <mhelmetag@gmail.com>
2018-09-18 22:52:34 -0700

    fix versioning with explicit version, update install steps

--- gosurf.rb
+++ gosurf.rb
@@ -3,9 +3,11 @@ class Gosurf < Formula
   homepage "https://github.com/mhelmetag/gosurf"
   url "https://github.com/mhelmetag/gosurf/releases/download/0.0.4/gosurf_darwin_amd64"
   sha256 "e46634283c047c71c1aaec05a0fe66ac1504958a5b21b027a4a825641ac6ef13"
+  version "0.0.4"
 
   def install
-    bin.install "gosurf_darwin_amd64", "gosurf"
+    bin.install "gosurf_darwin_amd64"
+    mv bin/"gosurf_darwin_amd64", bin/"gosurf"
   end
 
   test do
