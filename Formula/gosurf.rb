21b0dd32ddc1d08b84c16eeaf9b6fa1d5ebe85ae
Max Helmetag <mhelmetag@gmail.com>
2019-03-31 14:54:27 -0700

    update gosurf to 1.0.0

--- Formula/gosurf.rb
+++ Formula/gosurf.rb
@@ -1,9 +1,9 @@
 class Gosurf < Formula
   desc "CLI Client for Surfline"
   homepage "https://github.com/mhelmetag/gosurf"
-  url "https://github.com/mhelmetag/gosurf/releases/download/0.0.4/gosurf_darwin_amd64"
-  sha256 "e46634283c047c71c1aaec05a0fe66ac1504958a5b21b027a4a825641ac6ef13"
-  version "0.0.4"
+  url "https://github.com/mhelmetag/gosurf/releases/download/1.0.0/gosurf_darwin_amd64"
+  sha256 "714c59a6c83d87302a3b491f1f24a8d7df4d472250cf7cfb16b99a293bfad442"
+  version "1.0.0"
 
   def install
     bin.install "gosurf_darwin_amd64"
