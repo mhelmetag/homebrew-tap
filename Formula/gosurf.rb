3912008336cc3c4b8e98433681710416001ac399
Max Helmetag <mhelmetag@gmail.com>
2020-05-29 19:24:15 -0700

    update gosurf to 2.0.1

--- Formula/gosurf.rb
+++ Formula/gosurf.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class Gosurf < Formula
-  desc "CLI Client for Surfline"
-  homepage "https://github.com/mhelmetag/gosurf"
-  url "https://github.com/mhelmetag/gosurf/releases/download/1.0.0/gosurf_darwin_amd64"
-  sha256 "714c59a6c83d87302a3b491f1f24a8d7df4d472250cf7cfb16b99a293bfad442"
-  version "1.0.0"
+  desc 'CLI Client for Surfline'
+  homepage 'https://github.com/mhelmetag/gosurf'
+  url 'https://github.com/mhelmetag/gosurf/releases/download/2.0.1/gosurf_darwin_amd64'
+  sha256 'd6d6aa73feaffe637b369bf871760343f35eb988'
+  version '2.0.1'
 
   def install
-    bin.install "gosurf_darwin_amd64"
-    mv bin/"gosurf_darwin_amd64", bin/"gosurf"
+    bin.install 'gosurf_darwin_amd64'
+    mv bin / 'gosurf_darwin_amd64', bin / 'gosurf'
   end
 
   test do
-    output = shell_output(bin/"gosurf --version")
+    output = shell_output(bin / 'gosurf --version')
     assert_match "gosurf version #{version}", output
   end
 end
