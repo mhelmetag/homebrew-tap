baf4f2d136f77a3d880d9b3d5db843d9bd6aefee
Max Helmetag <mhelmetag@gmail.com>
2020-05-29 19:29:06 -0700

    fix sha sum for gosurf 2.0.1

--- Formula/gosurf.rb
+++ Formula/gosurf.rb
@@ -4,7 +4,7 @@ class Gosurf < Formula
   desc 'CLI Client for Surfline'
   homepage 'https://github.com/mhelmetag/gosurf'
   url 'https://github.com/mhelmetag/gosurf/releases/download/2.0.1/gosurf_darwin_amd64'
-  sha256 'd6d6aa73feaffe637b369bf871760343f35eb988'
+  sha256 'c97cd6b59c19a41c5578d22b367372322020455a8c96f85425bc6ef95fe956d0'
   version '2.0.1'
 
   def install
