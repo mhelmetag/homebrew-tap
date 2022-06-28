eb9e718294f94640e2d6f7938f5b6cc6cf48b8d1
Max Helmetag <mhelmetag@gmail.com>
2022-06-27 23:35:02 -0700

    update gosurf to 3.0.0, use go to build from source

--- Formula/gosurf.rb
+++ Formula/gosurf.rb
@@ -3,13 +3,14 @@
 class Gosurf < Formula
   desc 'CLI Client for Surfline'
   homepage 'https://github.com/mhelmetag/gosurf'
-  url 'https://github.com/mhelmetag/gosurf/releases/download/2.3.1/gosurf_darwin_amd64'
-  sha256 'adc9a8d7a772676a1e9fc3192605a68925df768ca02ff879763b1147d2cca984'
-  version '2.3.1'
+  url 'https://github.com/mhelmetag/gosurf/archive/refs/tags/3.0.0.tar.gz'
+  sha256 '8c7b22430da3516359bef0a7ba6f516edfd370025e1a74ff18760c8a3f7c1552'
+  version '3.0.0'
+
+  depends_on "go" => :build
 
   def install
-    bin.install 'gosurf_darwin_amd64'
-    mv bin / 'gosurf_darwin_amd64', bin / 'gosurf'
+    system "go", "build", *std_go_args
   end
 
   test do
